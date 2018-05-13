014c3e2242764a5890433597217c19f671f66285
Joseph Brown <josebrwn@gmail.com>
2018-05-13 08:43:12 -0700

    clean up

--- README.md
+++ README.md
@@ -2,12 +2,10 @@
 
 ## Intro
 
-This is a Node.js coding challenge I received recently*.
+This is a Node.js coding challenge I came across recently.
 
 In this project, the developer is asked to write a function that passes a set of tests found in `test/test.js`. These tests *expect* to receive a set of JSON instructions describing how to redistribute funds between people, according to ratios provided by the user.
 
 This is a fun example of a back-end coding challenge in Javascript that also demonstrates the dilemmas a developer faces when defending a multi-hour project like this. Javascript projects are a universe: just when should you begin to stop, and step away from a solution?
 
-This particular answer appeals to the principles of **Document First, Test Driven Development**. While there may be innumerable ways in which the solution can be improved, the tests are passing and therefore the code is complete.
-
-(*I should also note that while this solution was not accepted, the interviewer had never heard of `strict` mode, either.)
+The naive answer simply appeals to the principles of **Document First, Test Driven Development**. While there may be innumerable ways in which the solution can be improved, the tests are passing and therefore the code is complete.
--- generate-transfer-instructions.js
+++ generate-transfer-instructions.js
@@ -2,30 +2,7 @@ const _ = require('lodash') // Feel free to use
 const BigNumber = require('bignumber.js') // Feel free to use
 
 /**
-  * Given a set of users, a pool of funds, and a target distribution for those funds, this produces a set of instructions that sends funds between users so that the target distribution is met.
-  * For example, if "Seth" has a balance of 10, "Amy" a balance of 3, and "John" a balance of "1", if the ideal ratio between them is to have 1/3 each of the total (12), Seth will have to give Amy 2 and Seth will have to give John 3 so that each of them has 4, or 33% of the total.
-  *
-  * @param {Object} distribution - The ideal distribution. Keys are names, values are the fraction of the total distributed to that person.
-  *   @param {number} distribution.{name} - the distribution of the total that the person specified by 'name' should posess.
-  * @param {Object} balances - The current balance of each person. Keys are names, values are the current balance.
-  *   @param {string} balances.{name} - The current balance of the person specified by 'name.'
-  * @return {Array} - an array of `instruction` Objects
-  *   @return {Object} instruction
-  *     @return {string} instruction.from - the name of the person giving the amount
-  *     @return {string} instruction.to - the name of the person receiving the amount
-  *     @return {number} instruction.amount - the amount necessary so that ratios match
-  *
-  * @example:
-    const distribution = {
-      seth: "1/3",
-      amy: "1/3",
-      john: "1/3"
-    }
-    const balances = {
-      seth: 9,
-      amy: 2,
-      john: 1
-    }
+  * @example
     generateTransferInstructions(distribution, balances)
     # => [{
       from: "seth",
@@ -97,7 +74,7 @@ function generateTransferInstructions(distribution, balances){
     offset = _.sumBy(users, function(u) { return Math.abs(u.offset); });
 
   }
-  while (offset >=2) // 2 is the minimum solveable offset if units are integers.
+  while (offset >=2); // 2 is the minimum solveable offset if units are integers.
 
   console.log(instructions);
   
