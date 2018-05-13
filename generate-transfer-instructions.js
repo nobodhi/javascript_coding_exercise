7c1427ef9f00d74725728c997826b1cdf395a949
Joseph Brown <josebrwn@gmail.com>
2018-05-13 14:39:26 -0700

    added eslint

--- generate-transfer-instructions.js
+++ generate-transfer-instructions.js
@@ -1,46 +1,50 @@
-const _ = require('lodash') // Feel free to use
-const BigNumber = require('bignumber.js') // Feel free to use
+const _ = require('lodash');
+// const BigNumber = require('bignumber.js'); 
 
 /**
+ TODO: this module should pass the tests in /test/test.js. A naive solution is included below.
+
   * @example
-    generateTransferInstructions(distribution, balances)
-    # => [{
-      from: "seth",
-      to: "amy",
-      amount: 2
-    },{
-      from: "seth",
-      to: "john",
-      amount: 3
-    }]
- */
-function generateTransferInstructions(distribution, balances){
-  var instructions = [];
-  var users = []; 
+  generateTransferInstructions(distribution, balances)
+  # => [{
+    from: "seth",
+    to: "amy",
+    amount: 2
+  },{
+    from: "seth",
+    to: "john",
+    amount: 3
+  }]
+*/
+function generateTransferInstructions(distribution, balances) {
+  // set up variables for the two users, the total balances and the transfer instructions.
+  const instructions = [];
+  let users = [];
 
   // create the users array
-  Object.keys(distribution).forEach(function(key) {
-    var u = {name: key, fraction: distribution[key]};
+  Object.keys(distribution).forEach((key) => {
+    const u = { name: key, fraction: distribution[key] };
     users.push(u);
   });
 
   // append the users balance and fractional percent
-  Object.keys(balances).forEach(function(key) {
-    var u = _.find(users, {name:key});
+  Object.keys(balances).forEach((key) => {
+    const u = _.find(users, { name: key });
     u.balance = balances[key];
-    u.percent = eval(u.fraction);
+    u.percent = eval(u.fraction); // TODO no eval.
   });
 
   // use this to immediately sum a column.
-  var balance = _.sumBy(users, function(u) { return u.balance; }); 
+  const balance = _.sumBy(users, (u) => { return u.balance; }); // TODO arrow-body-style
+  let offset; // this is the inbalance pending, expressed as an offset from zero.
 
   // calculate the offset for each user based on their balance and their target percentage
-  Object.keys(users).forEach(function(key) {
+  Object.keys(users).forEach((key) => {
     users[key].target = (balance * users[key].percent);
     users[key].offset = users[key].balance - users[key].target;
   });
 
-  var offset = _.sumBy(users, function(u) { return Math.abs(u.offset); });  // if we get back to 0 we're done.
+  offset = _.sumBy(users, (u) => { return Math.abs(u.offset); }); // if we get back to 0 we're done.
 
   // **********************
   // algo: loop until all offsets are zero or remain unchanged (offsets<2).
@@ -52,33 +56,30 @@ function generateTransferInstructions(distribution, balances){
   do {
     // the donor has the largest offset.
     users = _.sortBy(users, 'offset'); 
-    var user1 = users[users.length - 1];
-    
+    const user1 = users[users.length - 1];
     // the recipient has the greatest negative offset
-    var user2 = _.chain(users).filter(u => u.offset < 0).first().value(); 
+    const user2 = _.chain(users).filter(u => u.offset < 0).first().value(); 
 
     // consruct the xfer insructions
-    var i = {};
+    const i = {};
     i.from = user1.name;
     i.to = user2.name;
     i.amount = user1.offset > Math.abs(user2.offset) ? Math.abs(user2.offset) : user1.offset;
     instructions.push(i);
 
     // adjust balances and offsets
-    user1.balance = user1.balance - i.amount;
-    user2.balance = user2.balance + i.amount;
-    user1.offset = user1.offset - i.amount;
-    user2.offset = user2.offset + i.amount;
+    user1.balance -= i.amount;
+    user2.balance += i.amount;
+    user1.offset -= i.amount;
+    user2.offset += i.amount;
 
     // re-compute total offset
-    offset = _.sumBy(users, function(u) { return Math.abs(u.offset); });
+    offset = _.sumBy(users, (u) => { return Math.abs(u.offset); });
 
   }
-  while (offset >=2); // 2 is the minimum solveable offset if units are integers.
-
-  console.log(instructions);
-  
-  return(instructions); // return the topmost transfer instructions 
+  while (offset >= 2); // HACK. 2 is the minimum solveable offset if units are integers.
+  console.log(instructions); // TODO no-console
+  return (instructions); // return the topmost transfer instructions
 }
 
 module.exports = generateTransferInstructions;
--- test/test.js
+++ test/test.js
@@ -1,111 +1,114 @@
-const Lab = require('lab')
-const Code = require('code')
-const lab = exports.lab = Lab.script()
-const describe = lab.describe
-const it = lab.it
-const expect = Code.expect
-const generateTransferInstructions = require('../generate-transfer-instructions.js')
+const Lab = require('lab');
+const Code = require('code');
+// TODO prefer-desctructuring, no-multi-assign
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+const generateTransferInstructions = require('../generate-transfer-instructions.js');
 
-describe('Generate Transfer Instructions', function(){
-  it('should generate the correct transfer instructions for a one to any redistribution', function(done){
+describe('Generate Transfer Instructions', () => {
+  it('should generate the correct transfer instructions for a one to any redistribution', (done) => {
     const distribution = {
-      seth: "1/3",
-      amy:  "1/3",
-      john: "1/3"
-    }
+      seth: '1/3',
+      amy: '1/3',
+      john: '1/3', // TODO comma-dangle (?)
+    };
     const balances = {
       seth: 9,
-      amy:  2,
-      john: 1
-    }
+      amy: 2,
+      john: 1,
+    };
     const expected = [{
-      from: "seth",
-      to:   "amy",
-      amount: 2
-    },{
-      from: "seth",
-      to:   "john",
-      amount: 3
-    }]
+      from: 'seth',
+      to: 'amy',
+      amount: 2,
+    }, 
+    {
+      from: 'seth',
+      to: 'john',
+      amount: 3,
+    }];
 
-    const result = generateTransferInstructions(distribution, balances)
+    const result = generateTransferInstructions(distribution, balances);
 
-    expect(result.length).to.equal(2)
+    expect(result.length).to.equal(2);
 
     // result array can be in any order, so check transfer instructions are present in the result
-    expect(expected).to.include(result[0])
-    expect(expected).to.include(result[1])
+    expect(expected).to.include(result[0]);
+    expect(expected).to.include(result[1]);
 
-    done()
-  })
+    done();
+  });
 
-  it('should generate the correct transfer instructions for an any to one redistribution', function(done){
+  it('should generate the correct transfer instructions for an any to one redistribution', (done) => {
     const distribution = {
-      seth: "1/3",
-      amy:  "1/3",
-      john: "1/3"
+      seth: '1/3',
+      amy: '1/3',
+      john: '1/3',
     }
     const balances = {
       seth: 5,
-      amy:  2,
-      john: 5
+      amy: 2,
+      john: 5,
     }
     const expected = [{
-      from: "seth",
-      to:   "amy",
-      amount: 1
+      from: 'seth',
+      to: 'amy',
+      amount: 1,
     },{
-      from: "john",
-      to:   "amy",
-      amount: 1
-    }]
+      from: 'john',
+      to: 'amy',
+      amount: 1,
+    }];
 
-    const result = generateTransferInstructions(distribution, balances)
+    const result = generateTransferInstructions(distribution, balances);
 
-    expect(result.length).to.equal(2)
+    expect(result.length).to.equal(2);
 
     // result array can be in any order, so check transfer instructions are present in the result
-    expect(expected).to.include(result[0])
-    expect(expected).to.include(result[1])
+    expect(expected).to.include(result[0]);
+    expect(expected).to.include(result[1]);
 
-    done()
-  })
-  it('should generate the correct transfer instructions for an any to any redistribution', function(done){
+    done();
+  });
+
+  it('should generate the correct transfer instructions for an any to any redistribution', (done) => {
     const distribution = {
-      seth: "1/3",
-      amy:  "1/6",
-      john: "1/3",
-      sue:  "1/6"
+      seth: '1/3',
+      amy: '1/6',
+      john: '1/3',
+      sue: '1/6',
     }
     const balances = {
       seth: 0,
-      amy:  4,
+      amy: 4,
       john: 3,
-      sue:  5
+      sue: 5,
     }
     const expected = [{
-      from: "sue",
-      to:   "seth",
-      amount: 3
+      from: 'sue',
+      to: 'seth',
+      amount: 3,
     }, {
-      from: "amy",
-      to:   "john",
-      amount: 1
+      from: 'amy',
+      to: 'john',
+      amount: 1,
     }, {
-      from: "amy",
-      to:   "seth",
-      amount: 1
-    }]
+      from: 'amy',
+      to: 'seth',
+      amount: 1,
+    }];
 
-    const result = generateTransferInstructions(distribution, balances)
+    const result = generateTransferInstructions(distribution, balances);
 
-    expect(result.length).to.equal(3)
+    expect(result.length).to.equal(3);
 
     // result array can be in any order, so check transfer instructions are present in the result
-    expect(expected).to.include(result[0])
-    expect(expected).to.include(result[1])
-    expect(expected).to.include(result[2])
+    expect(expected).to.include(result[0]);
+    expect(expected).to.include(result[1]);
+    expect(expected).to.include(result[2]);
 
-    done()
-  })
-})
+    done();
+  });
+});
