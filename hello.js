99b1d4244f58b1ae5cf286591fef1eddd38aa8f9
Joseph Brown <josebrwn@gmail.com>
2018-05-22 13:30:21 -0700

    new files

---
+++ hello.js
@@ -0,0 +1,15 @@
+/**
+Common string compaure interview questions written in JS.
+
+@example
+// herp derp blah
+*/
+const greeting = 'hello ';
+
+// populates the nth element of the array and returns it
+function foo(e) {
+  const newGreeting = greeting + e;
+  return newGreeting;
+}
+
+module.exports = foo;
---
+++ string-compare.js
@@ -0,0 +1,18 @@
+/**
+Common string compaure interview questions written in JS.
+
+@example
+// herp derp blah
+*/
+const greeting1 = 'hello';
+const greeting2 = 'well hello there';
+
+exports.foo = (e) => {
+  const newGreeting = greeting1 + e;
+  return newGreeting;
+};
+
+exports.bar = (e) => {
+  const newGreeting = greeting2 + e;
+  return newGreeting;
+};
