b2ad88dafcac772bcf0734021ae7a26a6f6ee5fd
Joseph Brown <josebrwn@gmail.com>
2018-05-29 17:19:25 -0700

    converted hello-test.js to mocha/chai

--- test/hello-test.js
+++ test/hello-test.js
@@ -1,23 +1,22 @@
-const Lab = require('lab');
-const Code = require('code');
-
-const lab = exports.lab = Lab.script(); // TODO no-multi-assign, prefer-destructuring
-const describe = lab.describe;
-const it = lab.it;
-const expect = Code.expect;
+/*
+The expect interface provides function for assertion.
+The should interface extends each object with a should property for assertion.
+*/
+const expect = require('chai').expect;
+const should = require('chai').should();
 const hello = require('../hello.js'); // this is the module being tested
 
 describe('Hello world test', () => {
   it('should say "hello"', (done) => {
     const name = 'joe';
     const result = hello.shortGreeting(name);
-    expect(result).to.equal('hello joe');
+    expect(result).to.equal('hello joe'); // using expect
     done();
   });
   it('should say "well hello there"', (done) => {
     const name = 'joe';
     const result = hello.longGreeting(name);
-    expect(result).to.equal('well hello there joe');
+    (result).should.equal('well hello there joe'); // using should
     done();
   });
 });
