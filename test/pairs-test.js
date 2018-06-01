098ec5aa2dc2149ea9f5098b5382f756e9d17c6f
Joseph Brown <josebrwn@gmail.com>
2018-06-01 15:34:58 -0700

    added pairs test

---
+++ test/pairs-test.js
@@ -0,0 +1,32 @@
+/**
+test script for find-pairs coding challenge. a small data file is in data/input.txt
+Each line contains titles associated with a user. Find all pairs of such titles.
+*/
+const chai = require('chai');
+
+const expect = chai.expect;
+const assert = chai.assert;
+
+const findPairs = require('../find-pairs.js'); // this is the module being tested
+
+describe('Find pairs test', () => {
+  it('should find all eight results', (done) => {
+    const result = findPairs(1);
+    assert.equal(result.length, 8); // assert
+    done();
+  });
+  it('should find three pairs that occur at least two times', (done) => {
+    const result = findPairs(2);
+    assert.equal(result.length, 3); // assert
+    done();
+  });
+  it('should find one pair that occurs three times', (done) => {
+    const result = findPairs(3);
+    const expected = [{
+      pair: 'Eminem,Radiohead',
+      count: 3,
+    }];
+    expect(result).to.deep.equal(expected); // expect
+    done();
+  });
+});
