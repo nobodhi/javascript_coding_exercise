85fd5a8372502d887c9ed1db8df1bf0c535c9124
Joseph Brown <josebrwn@gmail.com>
2020-03-06 11:06:39 -0500

    randomized fibonacci unit test

--- fibonacci.js
+++ fibonacci.js
@@ -8,8 +8,8 @@
 function fib(e) {
   let result = [1]
   let x = y = 1
-  if (e < 2) 
-    return 1
+  if (e < 2 || !(Number.isInteger(e))) 
+    return [1]
   for (let i=1;i<e;i++) {
     [x,y] = [y,x+y]
     result.push(x)
@@ -17,8 +17,8 @@ function fib(e) {
   return result;
 }
 
-// testing
-// let e = 8;
+// // testing
+// let e = 0;
 // console.log(fib(e))
 
 module.exports = fib;
--- test/fib-test.js
+++ test/fib-test.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 // TODO add a test that N can only be a natural number.
 describe('Fibonacci sum of series test', () => {
   it('should return an array containing the first n+1 elements of the fibonacci sequence', (done) => {
-    const x = 8; // choose any natural number TODO
+    const x = Math.floor(Math.random() * 20)+1;
     const y = x + 2; // series sum of F(n) = F(n+2)−1
     const result = fibonacci(x);
     const series = result.reduce((arr, val) => { return arr + val; }); // series = sum of array
