40fa68ddd8cd0b47bb7267dffe66d3397ef697bf
Joseph Brown <josebrwn@gmail.com>
2020-03-10 12:46:45 -0400

    simplified sieve arrays; only search for remaining prime multiples

--- sieve.js
+++ sieve.js
@@ -5,22 +5,25 @@
  * @returns {Array} a list of primes numbers  
  */
 const sieve = (n) => {
-  const numbers = Array.from(Array(n).keys()).map(x=>++x) 
-  numbers.splice(0,1)
-  const sieve = Array.from(Array(Math.floor(Math.sqrt(n))+1).keys()).map(x=>++x)
-  sieve.splice(0,1)
+  const numbers = Array.from(Array(n+1).keys())
+  numbers.splice(0,2)
+  const s = Math.floor(Math.sqrt(n+1))
+  const sieve = Array.from(Array(s).keys())
+  sieve.splice(0,2)
   const primes = new Map(numbers.map(x => [x, true]))
   sieve.forEach(s => {
     /* 
     for every remaining number in the sieve, remove its square 
-    and every subsequent multiple up to sqrt(n)
-    */  
+    and every subsequent multiple up to sqrt(n) from the map
+    */
+    if (primes.get(s)) {
     for (let m=Math.pow(s, 2);m<=n;m+=s) {
-      if (primes.get(m))
-        primes.delete(m);
+      primes.delete(m);
+      }
     }
   })
   return Array.from(primes.keys());
 }
 
 console.log(sieve(200))
+module.exports = sieve;
