6829345f88a9bf23884616f8b8667f334045a1f2
Joseph Brown <josebrwn@gmail.com>
2020-03-10 11:56:33 -0400

    finished sieve.js

--- sieve.js
+++ sieve.js
@@ -10,17 +10,17 @@ const sieve = (n) => {
   const sieve = Array.from(Array(Math.floor(Math.sqrt(n))+1).keys()).map(x=>++x)
   sieve.splice(0,1)
   const primes = new Map(numbers.map(x => [x, true]))
-  /* 
-  for every number in the sieve, remove its square and every subsequent multiple up to sqrt(n)
-  e.g. (2): 4, 6, 8, 10, ...
-  (3): 9, 15, 21, 27, ...
-  (5): 25, 35, 55, ...
-  */
-  console.log(sieve)
-  console.log(numbers)
-  console.log(primes)
-  return primes
-
+  sieve.forEach(s => {
+    /* 
+    for every remaining number in the sieve, remove its square 
+    and every subsequent multiple up to sqrt(n)
+    */  
+    for (let m=Math.pow(s, 2);m<=n;m+=s) {
+      if (primes.get(m))
+        primes.delete(m);
+    }
+  })
+  return Array.from(primes.keys());
 }
 
-sieve(20)
+console.log(sieve(200))
